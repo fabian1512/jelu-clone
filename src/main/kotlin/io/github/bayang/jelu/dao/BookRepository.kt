7615b55f34f603a2bc9480c0e93d37c0e83ac557
fabian1512 <43601131+fabian1512@users.noreply.github.com>
2026-04-27 13:18:26 +0200

    fix: use selectAll() instead of slice for subquery

--- src/main/kotlin/io/github/bayang/jelu/dao/BookRepository.kt
+++ src/main/kotlin/io/github/bayang/jelu/dao/BookRepository.kt
@@ -484,10 +484,7 @@ class BookRepository(
         }
         if (libraryFilter != LibraryFilter.ANY && user?.id != null) {
             val userBookSubQuery =
-                UserBookTable
-                    .slice(UserBookTable.book)
-                    .selectAll()
-                    .where { UserBookTable.user eq user.id }
+                UserBookTable.selectAll().where { UserBookTable.user eq user.id }
             if (libraryFilter == LibraryFilter.ONLY_USER_BOOKS) {
                 query.andWhere { BookAuthors.book inSubQuery userBookSubQuery }
             } else if (libraryFilter == LibraryFilter.ONLY_NON_USER_BOOKS) {
