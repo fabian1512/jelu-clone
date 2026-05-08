725d157546ddc2cc075825ae9351f34bec185fd3
fabian1512 <43601131+fabian1512@users.noreply.github.com>
2026-05-08 19:01:52 +0200

    ui: unify grid layout on all book list pages (my-3 mt-2)

--- src/jelu-ui/src/components/Author/AuthorBooks.vue
+++ src/jelu-ui/src/components/Author/AuthorBooks.vue
@@ -431,7 +431,7 @@ getBooks()
     :per-page="perPage"
     @change="updatePage"
   />
-  <div class="grid gap-[12px] grid-cols-1 sm:grid-cols-3 md:grid-cols-[repeat(auto-fill,minmax(9rem,1fr))] mt-2">
+  <div class="grid gap-[12px] grid-cols-1 sm:grid-cols-3 md:grid-cols-[repeat(auto-fill,minmax(9rem,1fr))] my-3 mt-2">
     <div
       v-for="book in convertedBooks"
       :key="book.book.id"
--- src/jelu-ui/src/components/Misc/Welcome.vue
+++ src/jelu-ui/src/components/Misc/Welcome.vue
@@ -168,7 +168,7 @@ const { typographyClasses } = useTypography()
       >
         {{ t('home.currently_reading') }} :
       </h2>
-       <div class="grid gap-[12px] grid-cols-1 sm:grid-cols-3 md:grid-cols-[repeat(auto-fill,minmax(9rem,1fr))]">
+       <div class="grid gap-[12px] grid-cols-1 sm:grid-cols-3 md:grid-cols-[repeat(auto-fill,minmax(9rem,1fr))] my-3 mt-2">
         <div
           v-for="book in books"
           :key="book.id"
