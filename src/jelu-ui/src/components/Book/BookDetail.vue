62b83537395d98173a973cdc56adba3b764c15c3
fabian1512 <43601131+fabian1512@users.noreply.github.com>
2026-05-09 23:08:24 +0200

    BookDetail: add grid layout for consistent field heights

--- src/jelu-ui/src/components/Book/BookDetail.vue
+++ src/jelu-ui/src/components/Book/BookDetail.vue
@@ -735,36 +735,86 @@ getBook()
           </div>
         </figure>
       </div>
-      <div class="text-left">
-        <h3
-          class="text-xl sm:text-2xl md:text-3xl"
-          :class="typographyClasses"
-        >
-          {{ book?.book?.title }}
-        </h3>
-        <h4
-          v-if="book?.book.originalTitle"
-          :class="typographyClasses"
-        >
-          {{ book.book.originalTitle }}
-        </h4>
-        <p class="flex flex-wrap items-center gap-1">
-          <span class="font-semibold capitalize">{{ t('book.author', 2) }} :</span>
-          <span v-if="book?.book?.authors && book.book.authors.length > 0">
-            <span
-              v-for="author in book?.book?.authors"
-              :key="author.id"
-            >
-              <router-link
-                class="link hover:underline hover:decoration-4 hover:decoration-secondary"
-                :to="{ name: 'author-detail', params: { authorId: author.id } }"
+      <div class="text-left grid grid-cols-1 sm:grid-cols-2 gap-y-2 gap-x-4">
+        <div class="contents">
+          <h3
+            class="text-xl sm:text-2xl md:text-3xl col-span-1 sm:col-span-2"
+            :class="typographyClasses"
+          >
+            {{ book?.book?.title }}
+          </h3>
+          <h4
+            v-if="book?.book.originalTitle"
+            class="col-span-1 sm:col-span-2"
+            :class="typographyClasses"
+          >
+            {{ book.book.originalTitle }}
+          </h4>
+          <p class="flex flex-wrap items-center gap-1 col-span-1 sm:col-span-2">
+            <span class="font-semibold capitalize">{{ t('book.author', 2) }} :</span>
+            <span v-if="book?.book?.authors && book.book.authors.length > 0">
+              <span
+                v-for="author in book?.book?.authors"
+                :key="author.id"
               >
-                {{ author.name }}
-              </router-link>
+                <router-link
+                  class="link hover:underline hover:decoration-4 hover:decoration-secondary"
+                  :to="{ name: 'author-detail', params: { authorId: author.id } }"
+                >
+                  {{ author.name }}
+                </router-link>
+              </span>
             </span>
-          </span>
-          <span v-else class="opacity-60">-</span>
-        </p>
+            <span v-else class="opacity-60">-</span>
+          </p>
+          <p class="col-span-1">
+            <span class="font-semibold capitalize">{{ t('book.publisher') }} :&nbsp;</span>
+            <router-link
+              v-if="book?.book?.publisher"
+              class="link hover:underline hover:decoration-4 hover:decoration-secondary"
+              :to="{ name: 'search', query: { q: `publisher:` + publisherQuery } }"
+            >
+              {{ book.book.publisher }}
+            </router-link>
+            <span v-else class="opacity-60">-</span>
+          </p>
+          <p class="col-span-1">
+            <span class="font-semibold uppercase">{{ t('book.isbn10') }} :</span>
+            <span :class="book?.book?.isbn10 ? '' : 'opacity-60'">{{ book?.book?.isbn10 || '-' }}</span>
+          </p>
+          <p class="col-span-1">
+            <span class="font-semibold uppercase">{{ t('book.isbn13') }} :</span>
+            <span :class="book?.book?.isbn13 ? '' : 'opacity-60'">{{ book?.book?.isbn13 || '-' }}</span>
+          </p>
+          <p class="col-span-1">
+            <span class="font-semibold capitalize">{{ t('book.page', 2) }} :</span>
+            <span v-if="book?.book?.pageCount || book?.currentPageNumber">
+              <span v-if="book?.book?.pageCount">{{ book.book.pageCount }}</span>
+              <span v-if="book?.currentPageNumber">&nbsp;(<span class="font-semibold capitalize">{{ t('labels.current') }}</span> : {{ book.currentPageNumber }})</span>
+            </span>
+            <span v-else-if="book?.percentRead != null" class="capitalize">
+              {{ t('book.percent_read') }} {{ book.percentRead }} %
+            </span>
+            <span v-else class="opacity-60">-</span>
+          </p>
+          <p class="col-span-1">
+            <span class="font-semibold capitalize">{{ t('book.published_date') }} :</span>
+            <span v-if="book?.book?.publishedDate">{{ d(stringToDate(book.book.publishedDate) ?? '', 'short') }}</span>
+            <span v-else class="opacity-60">-</span>
+          </p>
+          <p class="font-semibold capitalize col-span-1 sm:col-span-2">
+            {{ t('book.summary') }} :
+          </p>
+          <div
+            v-if="book?.book?.summary"
+            class="h-48 overflow-y-auto text-left col-span-1 sm:col-span-2"
+          >
+            <p v-html="displaySummary" />
+          </div>
+          <div v-else class="h-48 overflow-y-auto text-left opacity-60 col-span-1 sm:col-span-2">
+            -
+          </div>
+        </div>
         <p
           v-if="book != null && book.book != null && book.book.translators != null && book?.book?.translators?.length > 0"
         >
